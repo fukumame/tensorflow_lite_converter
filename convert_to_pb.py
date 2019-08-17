0cb59918c880190e6c2e529f0a2a3135ba8c990d
fukumame <tanuma0115@gmail.com>
2019-08-17 22:34:16 +0900

    wip

--- convert_to_pb.py
+++ convert_to_pb.py
@@ -5,7 +5,7 @@ from onnx_tf.backend import prepare
 def main():
     model_path = "models/yolo_v3.onnx"
     onnx_model = onnx.load(model_path)
-    tf_exp = prepare(onnx_model)
+    tf_exp = prepare(onnx_model, device='CPU')
     print(tf_exp.tensor_dict)
     tf_exp.export_graph("models/yolo_v3.pb")
 
